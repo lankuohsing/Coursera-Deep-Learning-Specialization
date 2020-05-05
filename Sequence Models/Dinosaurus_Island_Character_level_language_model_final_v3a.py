464de1e213ad83f7ba56fa8b169e7593a90cd23c
lankuohsing <lankuohsing@163.com>
2020-05-06 01:06:50 +0800

    add multi-dimension np array examples

---
+++ Sequence Models/Dinosaurus_Island_Character_level_language_model_final_v3a.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 23:39:47 2020
+
+@author: lankuohsing
+"""
+
+# In[]
+import numpy as np
+from utils import *
+import random
+import pprint
+# In[]
+data = open('dinos.txt', 'r').read()
+data= data.lower()
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
+
+# In[]
+chars = sorted(chars)
+print(chars)
+# In[]
+char_to_ix = { ch:i for i,ch in enumerate(chars) }
+ix_to_char = { i:ch for i,ch in enumerate(chars) }
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(ix_to_char)
+# In[]
+### GRADED FUNCTION: clip
+
+def clip(gradients, maxValue):
+    '''
+    Clips the gradients' values between minimum and maximum.
+
+    Arguments:
+    gradients -- a dictionary containing the gradients "dWaa", "dWax", "dWya", "db", "dby"
+    maxValue -- everything above this number is set to this number, and everything less than -maxValue is set to -maxValue
+
+    Returns:
+    gradients -- a dictionary with the clipped gradients.
+    '''
+
+    dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients['dby']
+
+    ### START CODE HERE ###
+    # clip to mitigate exploding gradients, loop over [dWax, dWaa, dWya, db, dby]. (≈2 lines)
+    for gradient in [dWaa, dWax, dWya, db, dby]:
+        np.clip(gradient,a_min=-maxValue,a_max=maxValue,out=gradient)
+    ### END CODE HERE ###
+
+    gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
+
+    return gradients
+# In[]
+# Test with a maxvalue of 10
+maxValue = 10
+np.random.seed(3)
+dWax = np.random.randn(5,3)*10
+dWaa = np.random.randn(5,5)*10
+dWya = np.random.randn(2,5)*10
+db = np.random.randn(5,1)*10
+dby = np.random.randn(2,1)*10
+gradients = {"dWax": dWax, "dWaa": dWaa, "dWya": dWya, "db": db, "dby": dby}
+gradients = clip(gradients, maxValue)
+print("gradients[\"dWaa\"][1][2] =", gradients["dWaa"][1][2])
+print("gradients[\"dWax\"][3][1] =", gradients["dWax"][3][1])
+print("gradients[\"dWya\"][1][2] =", gradients["dWya"][1][2])
+print("gradients[\"db\"][4] =", gradients["db"][4])
+print("gradients[\"dby\"][1] =", gradients["dby"][1])
+
+# In[]
+# Test with a maxValue of 5
+maxValue = 5
+np.random.seed(3)
+dWax = np.random.randn(5,3)*10
+dWaa = np.random.randn(5,5)*10
+dWya = np.random.randn(2,5)*10
+db = np.random.randn(5,1)*10
+dby = np.random.randn(2,1)*10
+gradients = {"dWax": dWax, "dWaa": dWaa, "dWya": dWya, "db": db, "dby": dby}
+gradients = clip(gradients, maxValue)
+print("gradients[\"dWaa\"][1][2] =", gradients["dWaa"][1][2])
+print("gradients[\"dWax\"][3][1] =", gradients["dWax"][3][1])
+print("gradients[\"dWya\"][1][2] =", gradients["dWya"][1][2])
+print("gradients[\"db\"][4] =", gradients["db"][4])
+print("gradients[\"dby\"][1] =", gradients["dby"][1])
+
+# In[]
+import numpy as np
+matrix1 = np.array([[1,1],[2,2],[3,3]]) # (3,2)
+matrix2 = np.array([[0],[0],[0]]) # (3,1)
+vector1D = np.array([1,1]) # (2,)
+vector2D = np.array([[1],[1]]) # (2,1)
+print("matrix1 \n", matrix1,"\n")
+print("matrix2 \n", matrix2,"\n")
+print("vector1D \n", vector1D,"\n")
+print("vector2D \n", vector2D)
+# In[]
+print("Multiply 2D and 1D arrays: result is a 1D array\n",
+      np.dot(matrix1,vector1D))
+print("Multiply 2D and 2D arrays: result is a 2D array\n",
+      np.dot(matrix1,vector2D))
+# In[]
+print("Adding (3 x 1) vector to a (3 x 1) vector is a (3 x 1) vector\n",
+      "This is what we want here!\n",
+      np.dot(matrix1,vector2D) + matrix2)
+# In[]
+print("Adding a (3,) vector to a (3 x 1) vector\n",
+      "broadcasts the 1D array across the second dimension\n",
+      "Not what we want here!\n",
+      np.dot(matrix1,vector1D) + matrix2
+     )
