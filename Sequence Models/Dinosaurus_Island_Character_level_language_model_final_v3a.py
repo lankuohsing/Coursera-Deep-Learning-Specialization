01bb644ffb4373c63b0647449ea3068649692b65
lankuohsing <lankuohsing@163.com>
2020-05-08 00:02:55 +0800

    add dinosaur

--- Sequence Models/Dinosaurus_Island_Character_level_language_model_final_v3a.py
+++ Sequence Models/Dinosaurus_Island_Character_level_language_model_final_v3a.py
@@ -206,3 +206,179 @@ print("Sampling:")
 print("list of sampled indices:\n", indices)
 print("size indices:\n", len(indices))
 print("list of sampled characters:\n", [ix_to_char[i] for i in indices])
+
+# In[]
+# GRADED FUNCTION: optimize
+
+def optimize(X, Y, a_prev, parameters, learning_rate = 0.01):
+    """
+    Execute one step of the optimization to train the model.
+
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+                        db -- Gradients of bias vector, of shape (n_a, 1)
+                        dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
+
+    ### START CODE HERE ###
+
+    # Forward propagate through time (≈1 line)
+    loss, cache = rnn_forward(X, Y, a_prev, parameters)
+
+    # Backpropagate through time (≈1 line)
+    gradients, a = rnn_backward(X, Y, parameters, cache)
+
+    # Clip your gradients between -5 (min) and 5 (max) (≈1 line)
+    gradients = clip(gradients, 5)
+
+    # Update parameters (≈1 line)
+    parameters = update_parameters(parameters, gradients, learning_rate)
+
+    ### END CODE HERE ###
+
+    return loss, gradients, a[len(X)-1]
+# In[]
+np.random.seed(1)
+vocab_size, n_a = 27, 100
+a_prev = np.random.randn(n_a, 1)
+Wax, Waa, Wya = np.random.randn(n_a, vocab_size), np.random.randn(n_a, n_a), np.random.randn(vocab_size, n_a)
+b, by = np.random.randn(n_a, 1), np.random.randn(vocab_size, 1)
+parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b, "by": by}
+X = [12,3,5,11,22,3]
+Y = [4,14,11,22,25, 26]
+loss, gradients, a_last = optimize(X, Y, a_prev, parameters, learning_rate = 0.01)
+print("Loss =", loss)
+print("gradients[\"dWaa\"][1][2] =", gradients["dWaa"][1][2])
+print("np.argmax(gradients[\"dWax\"]) =", np.argmax(gradients["dWax"]))
+print("gradients[\"dWya\"][1][2] =", gradients["dWya"][1][2])
+print("gradients[\"db\"][4] =", gradients["db"][4])
+print("gradients[\"dby\"][1] =", gradients["dby"][1])
+print("a_last[4] =", a_last[4])
+
+
+# In[]
+# GRADED FUNCTION: model
+
+def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, dino_names = 7, vocab_size = 27):
+    """
+    Trains the model and generates dinosaur names.
+
+    Arguments:
+    data -- text corpus
+    ix_to_char -- dictionary that maps the index to a character
+    char_to_ix -- dictionary that maps a character to an index
+    num_iterations -- number of iterations to train the model for
+    n_a -- number of units of the RNN cell
+    dino_names -- number of dinosaur names you want to sample at each iteration.
+    vocab_size -- number of unique characters found in the text (size of the vocabulary)
+
+    Returns:
+    parameters -- learned parameters
+    """
+
+    # Retrieve n_x and n_y from vocab_size
+    n_x, n_y = vocab_size, vocab_size
+
+    # Initialize parameters
+    parameters = initialize_parameters(n_a, n_x, n_y)
+
+    # Initialize loss (this is required because we want to smooth our loss)
+    loss = get_initial_loss(vocab_size, dino_names)
+
+    # Build list of all dinosaur names (training examples).
+    with open("dinos.txt") as f:
+        examples = f.readlines()
+    examples = [x.lower().strip() for x in examples]
+
+    # Shuffle list of all dinosaur names
+    np.random.seed(0)
+    np.random.shuffle(examples)
+
+    # Initialize the hidden state of your LSTM
+    a_prev = np.zeros((n_a, 1))
+
+    # Optimization loop
+    for j in range(num_iterations):
+
+        ### START CODE HERE ###
+
+        # Set the index `idx` (see instructions above)
+        idx = j%len(examples)
+
+        # Set the input X (see instructions above)
+        single_example = examples[idx]
+        single_example_chars = [c for c in single_example]
+        single_example_ix = [char_to_ix[c] for c in single_example_chars]
+        X = [None]+single_example_ix
+
+        # Set the labels Y (see instructions above)
+        ix_newline = char_to_ix["\n"]
+        Y = X[1:]+[ix_newline]
+
+        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
+        # Choose a learning rate of 0.01
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters, learning_rate = 0.01)
+
+        ### END CODE HERE ###
+
+        # Use a latency trick to keep the loss smooth. It happens here to accelerate the training.
+        loss = smooth(loss, curr_loss)
+
+        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
+        if j % 2000 == 0:
+
+            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
+
+            # The number of dinosaur names to print
+            seed = 0
+            for name in range(dino_names):
+
+                # Sample indices and print them
+                sampled_indices = sample(parameters, char_to_ix, seed)
+                print_sample(sampled_indices, ix_to_char)
+
+                seed += 1  # To get the same result (for grading purposes), increment the seed by one.
+
+            print('\n')
+
+    return parameters
+
+# In[]
+'''
+from __future__ import print_function
+from keras.callbacks import LambdaCallback
+from keras.models import Model, load_model, Sequential
+from keras.layers import Dense, Activation, Dropout, Input, Masking
+from keras.layers import LSTM
+from keras.utils.data_utils import get_file
+from keras.preprocessing.sequence import pad_sequences
+from shakespeare_utils import *
+import sys
+import io
+# In[]
+print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+
+model.fit(x, y, batch_size=128, epochs=1, callbacks=[print_callback])
+
+# In[]
+# Run this cell to try with different inputs without having to re-train the model
+generate_output()
+'''
+# In[]
